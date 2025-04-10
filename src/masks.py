a364c623061fe1a8c7e8b71ffcebb0cc24e45c77
kirill <kirill48108@gmail.com>
2025-04-10 13:37:53 +0300

    add src/masks.py

---
+++ src/masks.py
@@ -0,0 +1,23 @@
+from typing import Union
+
+card_number: str = input("Введите номер карты :")
+account_number: str = input("Введите номер счета :")
+"""Получаем данные от пользователя"""
+
+
+def get_mask_card_number(card_number: [str]) -> Union[str]:
+    """Возвращаем замаскированный номер карты"""
+    len_number = 16
+    if len(card_number) == len_number and card_number.isdigit():
+        return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    else:
+        return "Вы ввели некорректные данные ! "
+
+
+def get_mask_account(account_number: [str]) -> Union[str]:
+    """Возвращаем замаскированный номер счета"""
+    len_acc_number = 20
+    if len(account_number) == len_acc_number and account_number.isdigit():
+        return f"**{account_number[-4:]}"
+    else:
+        return "Вы ввели некорректные данные ! "
