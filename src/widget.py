4375e9c670372fbcd6ae40032e223feaa3af1a5d
kirill <kirill48108@gmail.com>
2025-04-12 21:21:50 +0300

    fixes

--- src/widget.py
+++ src/widget.py
@@ -1,13 +1,14 @@
-from src.masks import get_mask_card_number
-from src.masks import get_mask_account
-from delitime import delitime
+from datetime import datetime
 
-def mask_account_card (card_of_account: str) -> str:
-    """Функция принимает номер карты/счёта с указанием ее типа """
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(card_of_account: str) -> str:
+    """Функция принимает номер карты/счёта с указанием ее типа"""
     card_of_account_list = card_of_account.split()
-    if "Счёт" in card_of_account_list :
+    if "Счёт" in card_of_account_list:
         return f"Счёт {get_mask_account(card_of_account_list[1])}"
-    elif "MasterCard" or "Maestro" in card_of_account_list:
+    elif "MasterCard" in card_of_account_list or "Maestro" in card_of_account_list:
         return f"{card_of_account_list[0]} {get_mask_card_number(card_of_account_list[1])}"
     elif "Visa" in card_of_account_list:
         numbers_card = []
@@ -20,4 +21,7 @@ def mask_account_card (card_of_account: str) -> str:
         str_numbers_card = "".join(numbers_card)
         return f"{name_card[0]} {name_card[1]} {get_mask_card_number(str_numbers_card)}"
 
-
+    def get_date(my_date: str) -> str:
+        """Функция конвертирования даты"""
+        date_obj = datetime.strptime(my_date, "%Y-%m-%dT%H:%M:%S.%f")
+        return date_obj.strftime("%d.%m.%Y")
