ddda313dd4081d1eb1ee45382ae3e00873897b88
kirill <kirill48108@gmail.com>
2025-04-19 20:54:05 +0300

    Init

--- src/processing.py
+++ src/processing.py
@@ -2,11 +2,12 @@ from typing import Dict, List
 
 
 def filter_by_state(info_list: List[Dict], state: str = "EXECUTED") -> List[Dict]:
-    """The function returns the list of dictionaries
-    with 'EXECUTED' key state"""
+    """Функция принимает список словарей и значение для ключа state(по умолчанию 'EXECUTED').
+    возвращает новый список словарей, содержащий только те словари, у которых ключ state
+    соответствует указанному значению."""
     return [item for item in info_list if item.get("state") == state]
 
 
 def sort_by_date(info_list: List[Dict], reverse: bool = True) -> List[Dict]:
-    """The function returns a new list sorted by date"""
+    """Функция принимает список словарей и необязательный параметр, задающий порядок сортировки"""
     return sorted(info_list, key=lambda x: x["date"], reverse=True)
