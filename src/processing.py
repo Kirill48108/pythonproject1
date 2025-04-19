c11e980e17b7428ad475b9981e589b810d441d35
kirill <kirill48108@gmail.com>
2025-04-19 19:28:15 +0300

    Init

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,7 @@
 from typing import Dict, List
 
 
-def filter_by_state(info_list: List[Dict],
-                    state: str = 'EXECUTED') -> List[Dict]:
+def filter_by_state(info_list: List[Dict], state: str = "EXECUTED") -> List[Dict]:
     """The function returns the list of dictionaries
     with 'EXECUTED' key state"""
     return [item for item in info_list if item.get("state") == state]
