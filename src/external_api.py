0fc25ba7b6a335f6636bfc7475227d4b0b3c5021
kirill <kirill48108@gmail.com>
2025-05-20 11:48:51 +0300

    homework 12_1

--- src/external_api.py
+++ src/external_api.py
@@ -6,12 +6,22 @@ import requests
 
 my_api = os.getenv("API_KEY")
 
-def get_exch_rate(curr: str , my_api: str) -> float:
+def get_exch_rate(curr: str, amount: float) -> float:
     """Функция возвращает курс валюты по отношению к рублю, обращаясь к АПИ apilayer.com"""
-    headers: dict = {"apikey": my_api}
-    url_frmtted = "https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=" + curr + "&amount=15000"
+    headers: dict = {"apikey": "API_KEY"}
+    url_frmtted = "https://api.apilayer.com/exchangerates_data/convert?to=RUB&from=" + curr + f"&amount={amount}"
 
     response = requests.get(url_frmtted, headers=headers)
     result_pyth = json.loads(response.text)
     result = result_pyth["info"]["rate"]
-    return result
+    return  result
+
+def convert_transaction(transaction: dict) -> float:
+    """Функция конвертации"""
+    amount = transaction["operationAmount"]["amount"]
+    currency = transaction["operationAmount"]["currency"]["code"]
+    if currency == "RUB":
+        return amount
+    else:
+        return amount * get_exch_rate(currency)
+
--- test/test_external_api.py
+++ test/test_external_api.py
@@ -1,7 +1,48 @@
-
+from src.external_api import convert_transaction
 from unittest.mock import patch, MagicMock
 from src.external_api import get_exch_rate
 import json
+import unittest
+from unittest.mock import patch
+
+
+
+class TestConvertTransaction(unittest.TestCase):
+    @patch('src.external_api.get_exch_rate')
+    def test_convert_transaction_usd(self, mock_get_exch_rate):
+
+        mock_get_exch_rate.return_value = 75.0
+
+        transaction = {
+            "operationAmount": {
+                "amount": 100.0,
+                "currency": {
+                    "code": "USD"
+                }
+            }
+        }
+
+
+        result = convert_transaction(transaction)
+        self.assertEqual(result, 7500.0)
+
+    def test_convert_transaction_rub(self):
+        transaction = {
+            "operationAmount": {
+                "amount": 100.0,
+                "currency": {
+                    "code": "RUB"
+                }
+            }
+        }
+
+        result = convert_transaction(transaction)
+        self.assertEqual(result, 100.0)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
 @patch('requests.get')
 def test_get_exch_rate(mock_get):
 
@@ -9,3 +50,4 @@ def test_get_exch_rate(mock_get):
     mock_response.text = json.dumps({'info': {'rate': 88.833612}})
     mock_get.return_value = mock_response
     assert get_exch_rate('USD', 'тут_мой_ключ_API') == 88.833612
+
